0b0bd21ff2d9b049103ec87d28cc967e581034a4
halgoz <hassanalgoz@gmail.com>
2023-04-24 16:28:57 +0300

    Configure business rules

--- internal/app/transfer/rules.go
+++ internal/app/transfer/rules.go
@@ -5,16 +5,19 @@ import (
 	"time"
 
 	"github.com/hassanalgoz/swe/internal/common"
+	"github.com/spf13/viper"
 )
+
+var discountEligibilityDuration = viper.GetDuration("app.discount_eligibility_duration")
 
 func isEligibleForDiscount(c *common.Customer) (bool, string) {
 	// must has accounts
 	if !(len(c.Accounts) > 0) {
 		return false, "customer must have accounts"
 	}
-	// must be registered for at least 5 years
-	if !(time.Since(c.CreatedAt) > 5*365*24*time.Hour) {
-		return false, "customer must be registered for at least 5 years"
+	// must be registered for at least discountEligibilityDuration
+	if !(time.Since(c.CreatedAt) > discountEligibilityDuration) {
+		return false, fmt.Sprintf("customer must be registered for at least %v", discountEligibilityDuration)
 	}
 	return true, ""
 }
