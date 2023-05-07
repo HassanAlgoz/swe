fe5efa7cc2f5d0e0b3975a427d257bc47887e5b7
Hassan Algoz <hassanalgoz@gmail.com>
2023-05-07 11:56:04 +0300

    fix err not found

--- pkg/inbound/grpc/errors.go
+++ pkg/inbound/grpc/errors.go
@@ -1,15 +1,14 @@
 package grpc
 
 import (
-	"errors"
-
 	"github.com/hassanalgoz/swe/pkg/entities"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func ToStatusError(err error) error {
-	if errors.Is(err, entities.ErrNotFound) {
+	if e, ok := err.(*entities.ErrNotFound); ok {
+		return status.Error(codes.NotFound, e.Error())
 	} else if e, ok := err.(*entities.ErrInvalidArgument); ok {
 		return status.Error(codes.InvalidArgument, e.Error())
 	}
